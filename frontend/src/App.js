6a8f0d2cfc3e4ddf619b9cfc1a776951157dbc62
neo <neolawrencemasilo@gmail.com>
2024-11-16 22:31:03 +0200

    Side nav implementation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
-
+import { NavBar } from "./components/SME/NavBar";
+import { Dashboard } from "./pages/SME/Dashboard"
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <div className="flex flex-row h-screen w-[100%] bg-[#E2E2E2]" style={{ fontFamily: '"Inter", serif' }}>
+      <div className="w-[240px] bg-[#F0F5F7] opacity-[100%] h-full">
+        <NavBar />
+      </div>
+
+      <div className="w-[100%] m-[15px] bg-white ">
+        <Dashboard />
+      </div>
+  </div>
   );
 }
 
