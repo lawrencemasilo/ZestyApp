a534a142ef9ab03a18d6f966e5fede5540b79b93
Marlvin Kwenda <108397549+vinnywalker96@users.noreply.github.com>
2024-11-29 17:32:12 +0200

    Update authRoutes.js

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,11 +1,15 @@
 const express = require("express");
-const { registerUser, loginUser, getProfile } = require("../controllers/authController");
+const {
+  registerUser,
+  loginUser,
+  getUserProfile,
+} = require("../controllers/authController");
 const authMiddleware = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.get("/profile", authMiddleware, getProfile);
+router.get("/profile", authMiddleware, getUserProfile);
 
 module.exports = router;
