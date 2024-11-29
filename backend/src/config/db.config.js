15d666a7d503f7a303cf6a6d70ef6176047fbd27
Marlvin Kwenda <108397549+vinnywalker96@users.noreply.github.com>
2024-11-29 18:15:38 +0200

    Update db.config.js

--- backend/src/config/db.config.js
+++ backend/src/config/db.config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config(); // Load environment variables from .env file
 
 const connectDB = async () => {
-  const uri = process.env.MONGO_URI;
+  const uri = process.env.MONGO_URI || 'mongodb+srv://neolawrencemasilo:7YRs4NYHOvo7GhMF@cluster0.l9diw.mongodb.net/?'
 
   if (!uri) {
     console.error("MongoDB connection string is missing in environment variables");
