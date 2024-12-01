ccc805d85043b672ee2f56f156d6e3a853c69d72
neo <neolawrencemasilo@gmail.com>
2024-12-01 11:50:39 +0200

    implemented the account selection page

--- frontend/src/pages/Auth/AccountSelection.jsx
+++ frontend/src/pages/Auth/AccountSelection.jsx
@@ -1,8 +1,27 @@
-import React from "react";
-import { FaBuilding, FaUser } from "react-icons/fa";
-import { Factory, Store } from 'lucide-react';
+import React, { useState, useEffect } from "react";
+import { Factory, Store, CircleArrowLeft } from 'lucide-react';
+import money from "../../assets/images/money.jpg";
+import posman from "../../assets/images/posman.jpg";
+import pos from "../../assets/images/pos.jpg";
 
 const AccountSelection = () => {
+  const slides = [
+    money,
+    posman,
+    //pos,
+  ];
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  // Auto-slide logic
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prev) => (prev + 1) % slides.length);
+    }, 10000);
+
+    return () => clearInterval(interval); // Cleanup on component unmount
+  }, [slides.length]);
+
   return (
     <div className="flex flex-col md:flex-row h-screen w-full" style={{ fontFamily: '"Inter", serif' }}>
       {/* Left Section */}
@@ -10,17 +29,61 @@ const AccountSelection = () => {
         <div>
           <h1 className="text-[30px]">Zesty</h1>
         </div>
-        <div className="mt-[75px]">
-          <h1 className="text-[42px] font-bold mb-4">
-            Start Building Your Future with Zesty
-          </h1>
-          <p className="text-[16px] text-[#e1edff] font-thin mt-[20px]">
-          Are you a small-to-medium enterprise looking to access flexible financing solutions
-          or a supplier looking to streamline transactions with your SME partners?
-          <span className="text-[#e1edff] font-medium"> Select your role below to get started.</span>
-          </p>
+        <div className="flex flex-col w-full">
+          <div className="mt-[75px]">
+              <h1 className="text-[42px] font-bold mb-4">
+                Start Building Your Future with Zesty
+              </h1>
+              <p className="text-[16px] text-[#e1edff] font-thin mt-[20px]">
+              Are you a small-to-medium enterprise looking to access flexible financing solutions
+              or a supplier looking to streamline transactions with your SME partners?
+              <span className="text-[#e1edff] font-medium"> Select your role to get started.</span>
+              </p>
+            </div>
+            {/*<div className="relative w-[90%] h-[260px] max-w-md overflow-hidden">
+
+              <div className="flex items-center h-[240px]  rounded-lg p-1">
+
+                <div
+                  className="flex transition-transform duration-500 ease-in-out"
+                  style={{
+                    transform: `translateX(-${currentSlide * 100}%)`,
+                  }}
+                >
+                  {slides.map((src, index) => (
+                    <div
+                      key={index}
+                      className="min-w-full flex items-center justify-center"
+                    >
+                      <img
+                        src={src}
+                        alt={`Slide ${index + 1}`}
+                        className="rounded-lg shadow-lg w-3/4 h-[200px]"
+                      />
+                    </div>
+                  ))}
+                </div>
+              </div>
+
+              <div className="flex justify-center mt-1 space-x-2">
+                {slides.map((_, index) => (
+                  <button
+                    key={index}
+                    onClick={() => setCurrentSlide(index)}
+                    className={`w-2 h-2 rounded-full ${
+                      currentSlide === index ? "bg-blue-500" : "bg-gray-300"
+                    }`}
+                  ></button>
+                ))}
+              </div>
+            </div>*/}
+            <button className="flex flex-row items-center justify-center w-[100px] h-[45px] mt-8 border-[1px] border-white [#005EFF] hover:bg-[#295bb1] text-white text-sm px-3 py-2 rounded-lg">
+              <CircleArrowLeft className="text-white mr-[7px]" size={20} />
+              <span className="text-[16px]">Back</span>
+            </button>
+          </div>
         </div>
-      </div>
+        
       {/* Right Section */}
       <div className="flex flex-col justify-center bg-[#FAFBFC] p-14 md:w-[60%]">
         <h2 className="text-[26px] font-semibold mb-6 text-[#252F3F]">Choose Your Account Type</h2>
@@ -29,7 +92,7 @@ const AccountSelection = () => {
         </p>
         <div className="space-y-6 w-full max-w-md">
           {/* Business Account */}
-          <button className="flex items-center w-full bg-white hover:bg-gray-200 px-6 py-4 rounded-lg hover-account-select">
+          <button className="flex items-center w-full bg-white  px-6 py-4 rounded-lg hover-account-select">
             <div className="flex justify-center items-center w-[85px] h-[60px] border-[2px] border-[#005EFF] rounded-lg ">
               <Store className="text-[#005EFF] text-2xl" />   
             </div>
