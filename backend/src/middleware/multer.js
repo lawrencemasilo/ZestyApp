278221d229c8c77aa67e602974dc03d09fde8289
Sima <smambo85@gmail.com>
2025-01-21 20:37:03 +0200

    Updated multer logic

--- backend/src/middleware/multer.js
+++ backend/src/middleware/multer.js
@@ -1,4 +1,6 @@
 const multer = require('multer');
+const path = require('path');
+const fileFilter = require('../utils/fileFilter');
 
 // Multer configuration
 const storage = multer.diskStorage({
@@ -6,12 +8,15 @@ const storage = multer.diskStorage({
         cb(null, "uploads/"); // directory for storing files
     },
     filename: (req, file, cb) => {
-        cb(null, `${Date.now()}-${file.originalname}`); // file name
+        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9); // unique suffix
+        const ext = path.extname(file.originalname); // extract file extension
+        cb(null, `${file.fieldname}-${uniqueSuffix}${ext}`); // set file name
     },
 });
 
 const upload = multer({
     storage,
+    fileFilter,
     limits: { filesize: 16 * 1024 * 1024 }, // 16MB file size limit
 });
 
