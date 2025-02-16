eac524133e906d481f37787b08ff11d0d4a3ad96
Simamnkele <smambo85@gmail.com>
2025-02-16 22:15:38 +0200

    Minor changes to multer utility

--- backend/src/middleware/multer.js
+++ backend/src/middleware/multer.js
@@ -5,7 +5,7 @@ const fileFilter = require('../utils/fileFilter');
 // Multer configuration
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "uploads/"); // directory for storing files
+        cb(null, "./uploads/"); // directory for storing files
     },
     filename: (req, file, cb) => {
         const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9); // unique suffix
@@ -17,7 +17,7 @@ const storage = multer.diskStorage({
 const upload = multer({
     storage,
     fileFilter,
-    limits: { filesize: 16 * 1024 * 1024 }, // 16MB file size limit
+    limits: { fileSize: 16 * 1024 * 1024 }, // 16MB file size limit
 });
 
 module.exports = upload;
