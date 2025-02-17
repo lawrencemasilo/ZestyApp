7bfe1bdf27912683ca103d2591d8fc8cad701db7
Sima <smambo85@gmail.com>
2025-02-17 21:21:05 +0200

    Updated /documents endpoint logic

--- backend/src/routes/verificationRoutes.js
+++ backend/src/routes/verificationRoutes.js
@@ -2,16 +2,23 @@ const express = require("express");
 const upload = require("../middleware/multer");
 const router = express.Router();
 const { verifyDocuments, verifyBusiness } = require("../controllers/verificationController");
+const authMiddleware = require("../middleware/authMiddleware");
 
 // Verification Operations
-router.post("/documents", (req, res) => {
-    upload.array("documents", 5) (req, res, (err) => {
-        if (err) {
-            return res.status(400).json({ error: err.message });
-        }
-        verifyDocuments(req, res);
-    });
-});
+router.post(
+    "/documents",
+    authMiddleware,
+    upload.array("documents", 5),
+    (req, res, next) => {
+        //Handle multer errors
+        upload.array("documents", 5)(req, res, (err) => {
+            if (err) {
+                return res.status(400).json({ error: err.message });
+            }
+            verifyDocuments(req, res);
+        });
+    }
+);
 
 router.post("/business", verifyBusiness); // Verify business
 
